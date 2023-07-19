cc5d6bf15842bbbdaec47cb6aafada30ca1538b1
T4nae <tan.b63000@gmail.com>
2023-07-19 16:24:16 +0530

    entry points updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,95 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import { ScrollControls, Scroll } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useMemo, useState } from "react";
+import Scene from "./components/scene";
+import Hero from "./components/hero";
+import Nav from "./components/nav";
+import useDarkMode from "@/lib/darkmode";
+import Overview from "./components/overview";
+import { useMotionValue } from "framer-motion";
+import { A11yAnnouncer } from "@react-three/a11y";
+import home from "../../public/content/home";
+import Loader from "./components/loader";
+import References from "./components/references";
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+export default function HomePage() {
+    const { DarkMode, setDarkMode } = useDarkMode();
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    const Pages =
+        4 +
+        Math.ceil(home.Technologies.length * 0.9) +
+        Math.ceil(
+            (home.References.Projects.length +
+                home.References.Testimonials.length +
+                home.References.WorkExperience.length) *
+                0.9
+        );
+    const scrollYProgress = useMotionValue(0);
+    const Sections = useMemo(
+        () => [
+            {
+                name: "Island1",
+                start: 0,
+                end: 0.9,
+            },
+            {
+                name: "overview",
+                start: 0.06,
+                end: 0.2,
+            },
+            {
+                name: "SmallIslands",
+                start: 0.2,
+                end: 0.5,
+            },
+            {
+                name: "References",
+                start: 0.6,
+                end: 0.9,
+            },
+            {
+                name: "Island2",
+                start: 0.9,
+                end: 1,
+            },
+        ],
+        []
+    );
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <>
+            <Suspense fallback={<Loader />}>
+                <Canvas frameloop="demand">
+                    <ScrollControls pages={Pages}>
+                        <Scene
+                            Pages={Pages}
+                            Sections={Sections}
+                            scrollY={scrollYProgress}
+                            DarkMode={DarkMode}
+                            setDarkMode={setDarkMode}
+                        />
+                        <Scroll html>
+                            <div className="h-screen">
+                                <Nav
+                                    DarkMode={DarkMode}
+                                    setDarkMode={setDarkMode}
+                                />
+                                <Hero />
+                            </div>
+                            <div className="h-screen w-screen flex flex-row justify-end">
+                                <Overview
+                                    Section={Sections[1]}
+                                    scrollY={scrollYProgress}
+                                />
+                            </div>
+                            <References Section={Sections[3]} Pages={Pages} />
+                        </Scroll>
+                    </ScrollControls>
+                </Canvas>
+                <A11yAnnouncer />
+            </Suspense>
+        </>
+    );
 }
